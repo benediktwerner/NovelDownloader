ad2c843dfd724b3ac54cd22b87bc855d6ce37ca0
Benedikt Werner <1benediktwerner@gmail.com>
2019-01-14 21:42:23 +0100

    Force download encoding to utf-8

--- utils.py
+++ utils.py
@@ -30,6 +30,7 @@ def download_url(url, json=False, cookies=None):
     #     "User-Agent": 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',
     # }
     response = requests.get(url, cookies=cookies)
+    response.encoding = "utf-8"
 
     if json:
         return response.json()
